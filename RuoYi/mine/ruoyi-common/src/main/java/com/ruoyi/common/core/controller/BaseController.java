5314086fb68aba86bc2d1202362c7db9455b2c1e
lz958942 <404644381@qq.com>
2022-06-22 14:49:11 +0800

    功能扩展

--- RuoYi/mine/ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
+++ RuoYi/mine/ruoyi-common/src/main/java/com/ruoyi/common/core/controller/BaseController.java
@@ -115,11 +115,22 @@ public class BaseController
 
     /**
      * 响应返回结果
-     * 
+     *
      * @param rows 影响行数
      * @return 操作结果
      */
     protected AjaxResult toAjax(int rows)
+    {
+        return rows > 0 ? success() : error();
+    }
+
+    /**
+     * 响应返回结果
+     *
+     * @param rows 影响行数
+     * @return 操作结果
+     */
+    protected AjaxResult toAjax(long rows)
     {
         return rows > 0 ? success() : error();
     }
